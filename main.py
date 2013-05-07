a1562252cf3789d419d9b5cd6adac5af67b2b07b
Martin Eigenmann <eigenmannmartin@gmail.com>
2013-05-07 13:54:38 +0200

    add evo acs + tsp

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import csv
 import numpy as np
-#import cProfile as profile
+import cProfile as profile
 
 from traveling_santa import TSP
-from traveling_santa_evo import EVO
+from traveling_santa_evo import EVO as EVO
+from traveling_santa_evo_acs import EVO as ACS
 
 
 def read_data_file(file):
@@ -33,24 +34,64 @@ def subset_data(data, size):
         samples.append(data[i])
     return np.array(samples)
 
-
 if __name__ == '__main__':
     print "*** Step 1: ***"
     data = read_data_file('santa_cities.csv')  # id, x, y
-    data = subset_data(data, 15)
+    data = subset_data(data, 1500)
 
     print "*** Step 2: ***"
     tsp = TSP(data)
     tsp.solve()
 
     print "*** Step 3: ***"
-    evo = EVO(data)
+    route0 = tsp.h_tour
+
+    #evo = ACS(data,route0)
+    evo = EVO(data,route0)
+    #profile.run("evo.solve()")
     evo.solve()
 
+    route1 = evo.tour
+
+    route0_lenght = evo.calc_path_lenght(route0)
+    route1_lenght = evo.calc_path_lenght(route1)
+
     print "*** Step 4: ***"
+    print "# Duplicates: #"
+    douplicates = 0
+    for inx0, edge0 in enumerate(route0):
+        for inx1, edge1 in enumerate(route1):
+            if( (edge1[0] == edge0[0] and edge1[1] == edge0[1]) or (edge1[1] == edge0[0] and edge1[0] == edge0[1]) ):
+                print('duplicate ({0}) {1} and ({2}) {3}'.format(inx0, edge0, inx1, edge1))
+                douplicates += 1
+    print('dupicates: {0}'.format(douplicates) )
+    print('Path0: {0}'.format(evo.calc_path_lenght(route0)))
+    print('Path1: {0}'.format(evo.calc_path_lenght(route1)))
+
+    print('*** Step 4.1: ***')
+#opt
+    while(evo.calc_path_duplicates(route0,route1)):
+        for inx0, edge0 in enumerate(route0):
+            for inx1, edge1 in enumerate(route1):
+                if( (edge1[0] == edge0[0] and edge1[1] == edge0[1]) or (edge1[1] == edge0[0] and edge1[0] == edge0[1]) ):
+                    print('solving duplicate ({0}) {1} and ({2}) {3}'.format(inx0, edge0, inx1, edge1))
+                    if(evo.calc_path_lenght(route0)<evo.calc_path_lenght(route1)):
+                        print('manipulate route0 on {0}'.format(inx0) )
+                        route0.append( (route0[-1][1],route0[inx1][0]) )
+                        route0[inx0-1] = (route0[inx0-1][0],route0[inx0][1])
+                        route0.remove(route0[inx0])   
+                    else:
+                        print('manipulate route1 on {0}'.format(inx1) )
+                        route1.append( (route1[-1][1],route1[inx1][0]) )
+                        route1[inx1-1] = (route1[inx1-1][0],route1[inx1][1])
+                        route1.remove(route1[inx1]) 
+
+    print('Path0: {0}'.format(evo.calc_path_lenght(route0)))
+    print('Path0: {0}'.format(route0))
+    print('Path1: {0}'.format(evo.calc_path_lenght(route1)))
+    print('Path1: {0}'.format(route1))
+
+    print('*** Step 5: ***')
     print "Results:"
-    print "[Name]Algo: ", tsp.best_tour_len
-    print "evolutionary algo: ", evo.best_tour_len
-    tsp.plot(showMST=False, labelNodes=True)
-    evo.plot(showMST=False, labelNodes=False)
-    #profile.run('tsp.plot(showMST=True)')
+    print('Path0: {0} / {1}'.format(evo.calc_path_lenght(route0), route0_lenght))
+    print('Path1: {0} / {1}'.format(evo.calc_path_lenght(route1), route1_lenght))
---
+++ traveling_santa_evo_tsp.py
@@ -0,0 +1,96 @@
+import copy
+from inspyred import ec
+from inspyred.ec import emo
+from inspyred.ec import selectors
+from inspyred import swarm
+import itertools
+import math
+import random
+
+
+class TSP(object):
+  
+    def __init__(self, weights, route):
+        self.route = route
+        self.weights = weights
+        self.components = [swarm.TrailComponent((i, j), value=(1 / weights[i][j])) for i, j in itertools.permutations(range(len(weights)), 2)]
+        self.bias = 0.5
+        self.bounder = ec.DiscreteBounder([i for i in range(len(weights))])
+        self.maximize = True
+        self._use_ants = False
+        
+    def __repr__(self):
+        return self.__class__.__name__
+
+        
+    def __call__(self, *args, **kwargs):
+        candidate = [a for a in args]
+        fit = self.evaluator([candidate], kwargs)
+        return fit[0]
+
+
+    def generator(self, random, args):
+        """Return a candidate solution for an evolutionary computation."""
+        locations = [i for i in range(len(self.weights))]
+        random.shuffle(locations)
+        return locations
+    
+    def constructor(self, random, args):
+        self._use_ants = True
+        """Return a candidate solution for an ant colony optimization."""
+        candidate = []
+        while len(candidate) < len(self.weights) - 1:
+            # Find feasible components
+            feasible_components = []
+            if len(candidate) == 0:
+                feasible_components = self.components
+            elif len(candidate) == len(self.weights) - 1:
+                first = candidate[0]
+                last = candidate[-1]
+                feasible_components = [c for c in self.components if c.element[0] == last.element[1] and c.element[1] == first.element[0]]
+            else:
+                last = candidate[-1]
+                already_visited = [c.element[0] for c in candidate]
+                already_visited.extend([c.element[1] for c in candidate])
+                already_visited = set(already_visited)
+                feasible_components = [c for c in self.components if c.element[0] == last.element[1] and c.element[1] not in already_visited]
+            if len(feasible_components) == 0:
+                candidate = []
+            else:
+                # Choose a feasible component
+                if random.random() <= self.bias:
+                    next_component = max(feasible_components)
+                else:
+                    next_component = selectors.fitness_proportionate_selection(random, feasible_components, {'num_selected': 1})[0]
+                candidate.append(next_component)
+        return candidate
+    
+    def evaluator(self, candidates, args):
+#opt
+        """Return the fitness values for the given candidates."""
+        fitness = [0 for _ in range(len(candidates))]
+        douplicates = 1
+        if self._use_ants :
+            for inx, candidate in enumerate(candidates):
+                total = 0
+                for c in candidate:
+                    total += self.weights[c.element[0]][c.element[1]]
+                    #find duplicated edges
+                    for b in self.route:
+                        if( (b[0] == c.element[0] and b[1] == c.element[1]) or (b[1] == c.element[0] and b[0] == c.element[1]) ):
+                            douplicates += 1
+                            total += self.weights[c.element[0]][c.element[1]]*douplicates
+                            
+                last = (candidate[-1].element[1], candidate[0].element[0])
+                total += self.weights[last[0]][last[1]]
+                fitness[inx] = ( 1 / total )
+        else:
+            
+            for candidate in candidates:
+                total = 0
+                for src, dst in zip(candidate, candidate[1:] + [candidate[0]]):
+                    total += self.weights[src][dst]
+                fitness.append(1 / total)
+
+
+        return fitness
